c2ed7e7e35fdf9b9fb29d9c154ad58a1ed14451b
Chmouel Boudjnah <chmouel@chmouel.com>
2022-10-28 10:00:05 +0200

    add black and properly ci the task
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- Makefile
+++ Makefile
@@ -1,11 +1,17 @@
 all: generate
+PY_FILES := $(shell find . -type f -regex ".*py" -print)
+
 
 install:
 	poetry install || true
 
 lint: install
-	poetry run pylint ./hack/tekton-task-embed-script.py
-	poetry run mypy ./hack/tekton-task-embed-script.py
+	poetry run pylint $(PY_FILES)
+	poetry run mypy $(PY_FILES)
+
+fmt: install
+	poetry run black $(PY_FILES)
+
 
 generate:
 	poetry run ./hack/tekton-task-embed-script.py task/tekton-slack-task-status.yaml | tee tekton-slack-task-status.yaml
--- task/tekton-slack-task-status.py
+++ task/tekton-slack-task-status.py
@@ -29,9 +29,7 @@ class SlackNotificationError(Exception):
 
 def get_openshift_console_url(namespace: str) -> str:
     """Get the openshift console url for a namespace"""
-    cmd = (
-        "kubectl get route -n openshift-console console -o jsonpath='{.spec.host}'",
-    )
+    cmd = ("kubectl get route -n openshift-console console -o jsonpath='{.spec.host}'",)
     ret = subprocess.run(cmd, shell=True, check=True, capture_output=True)
     if ret.returncode != 0:
         raise SlackNotificationError(
@@ -43,7 +41,8 @@ def get_openshift_console_url(namespace: str) -> str:
 def check_label(label_eval: str, label_to_check: str) -> bool:
     """Check a label: if you get a string that has all the labels as specified
     by github, it will eval it and check if one contains the label_to_check"""
-    return bool([x for x in eval(label_eval) if x['name'] == label_to_check])  # pylint: disable=eval-used
+    # pylint: disable=eval-used
+    return bool([x for x in eval(label_eval) if x["name"] == label_to_check])
 
 
 def get_json_of_pipelinerun(pipelinerun: str) -> typing.Dict[str, typing.Dict]:
@@ -57,25 +56,27 @@ def get_json_of_pipelinerun(pipelinerun: str) -> typing.Dict[str, typing.Dict]:
 
 
 def check_status_of_pipelinerun(
-    log_prefix: str,
-    jeez: typing.Dict[str,
-                      typing.Dict]) -> (typing.List[str], typing.List[str]):
+    log_prefix: str, jeez: typing.Dict[str, typing.Dict]
+) -> typing.Tuple[typing.List[str], typing.List[str]]:
     """Check status of a pipelinerun using kubectl, we avoid the the Running
     ones since we run in finally, it will have a running ones"""
-    task_runs = jeez['status']['taskRuns']
+    task_runs = jeez["status"]["taskRuns"]
     failure = []
     success = []
 
-    pname = jeez['metadata']['name']
+    pname = jeez["metadata"]["name"]
     for task in task_runs.keys():
-        bname = task.replace(pname + "-", '')
-        bname = bname.replace("-" + bname.split("-")[-1], '')
+        bname = task.replace(pname + "-", "")
+        bname = bname.replace("-" + bname.split("-")[-1], "")
         if log_prefix != "":
             bname = f"<{log_prefix}/{bname}|{bname}>"
-        if bool([
-                x['message'] for x in task_runs[task]['status']['conditions']
-                if x['status'] != 'Running' and x['status'] == 'False'
-        ]):
+        if bool(
+            [
+                x["message"]
+                for x in task_runs[task]["status"]["conditions"]
+                if x["status"] != "Running" and x["status"] == "False"
+            ]
+        ):
             failure.append(bname)
         else:
             success.append(bname)
@@ -83,34 +84,36 @@ def check_status_of_pipelinerun(
     return failure, success
 
 
-def send_slack_message(webhook_url: str, subject: str, text: str,
-                       icon: str) -> str:
+def send_slack_message(webhook_url: str, subject: str, text: str, icon: str) -> str:
     """Send a slack message"""
     msg = {
-        "text":
-        subject,
-        "attachments": [{
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": text,
+        "text": subject,
+        "attachments": [
+            {
+                "blocks": [
+                    {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": text,
+                        },
+                        "accessory": {
+                            "type": "image",
+                            "image_url": icon,
+                            "alt_text": "From tekton with love",
+                        },
                     },
-                    "accessory": {
-                        "type": "image",
-                        "image_url": icon,
-                        "alt_text": "From tekton with love"
-                    }
-                },
-            ]
-        }]
+                ]
+            }
+        ],
     }
 
-    req = urllib.request.Request(webhook_url,
-                                 data=json.dumps(msg).encode(),
-                                 headers={"Content-type": "application/json"},
-                                 method="POST")
+    req = urllib.request.Request(
+        webhook_url,
+        data=json.dumps(msg).encode(),
+        headers={"Content-type": "application/json"},
+        method="POST",
+    )
     # TODO: Handle error?
     return urllib.request.urlopen(req).read().decode()
 
@@ -123,20 +126,24 @@ def main() -> int:
         "--failure-url-image",
         default=os.environ.get(
             "FAILURE_URL_IMAGE",
-            "https://publicdomainvectors.org/photos/21826-REMIX-ARRET.png"),
-        help="The icon of failure")
+            "https://publicdomainvectors.org/photos/21826-REMIX-ARRET.png",
+        ),
+        help="The icon of failure",
+    )
 
     parser.add_argument(
         "--success-url-image",
         default=os.environ.get(
-            "SUCCESS_URL_IMAGE",
-            "https://publicdomainvectors.org/photos/Checkmark.png"),
-        help="The icon of success")
+            "SUCCESS_URL_IMAGE", "https://publicdomainvectors.org/photos/Checkmark.png"
+        ),
+        help="The icon of success",
+    )
 
-    parser.add_argument("--failure-text",
-                        help="The text of the slack message when failure",
-                        default=os.environ.get("FAILURE_TEXT",
-                                               "CI has failed :cry:"))
+    parser.add_argument(
+        "--failure-text",
+        help="The text of the slack message when failure",
+        default=os.environ.get("FAILURE_TEXT", "CI has failed :cry:"),
+    )
 
     parser.add_argument(
         "--success-text",
@@ -144,33 +151,39 @@ def main() -> int:
         help="The text of the slack message when succes",
     )
 
-    parser.add_argument("--log-url",
-                        default=os.environ.get("LOG_URL"),
-                        help="Link to the log url")
+    parser.add_argument(
+        "--log-url", default=os.environ.get("LOG_URL"), help="Link to the log url"
+    )
 
     parser.add_argument(
         "--openshift",
         default=os.environ.get("OPENSHIFT"),
-        help=
-        "Wethere we are running on OpenShift and then auto construct the URL, set this to true"
+        help="Wethere we are running on OpenShift and then auto construct the URL, set this to true",
     )
 
     parser.add_argument(
         "--github-pull-label",
         default=os.environ.get("GITHUB_PULL_LABEL"),
-        help="pull_request.labels dict as get from tekton asa code")
+        help="pull_request.labels dict as get from tekton asa code",
+    )
 
-    parser.add_argument("--report-success",
-                        default=os.environ.get("REPORT_SUCCESS"),
-                        help="wether to report success as well")
+    parser.add_argument(
+        "--report-success",
+        default=os.environ.get("REPORT_SUCCESS"),
+        help="wether to report success as well",
+    )
 
-    parser.add_argument("--pipelinerun",
-                        default=os.environ.get("PIPELINERUN"),
-                        help="The pipelinerun to check the status on")
+    parser.add_argument(
+        "--pipelinerun",
+        default=os.environ.get("PIPELINERUN"),
+        help="The pipelinerun to check the status on",
+    )
 
-    parser.add_argument("--slack-webhook-url",
-                        default=os.environ.get("SLACK_WEBHOOK_URL"),
-                        help="Slack webhook URL")
+    parser.add_argument(
+        "--slack-webhook-url",
+        default=os.environ.get("SLACK_WEBHOOK_URL"),
+        help="Slack webhook URL",
+    )
 
     args = parser.parse_args()
     if args.label_to_check and args.github_pull_label:
@@ -181,9 +194,7 @@ def main() -> int:
             return 0
 
     if not args.pipelinerun:
-        print(
-            "error --pipelinerun need to be set via env env variable or other means."
-        )
+        print("error --pipelinerun need to be set via env env variable or other means.")
         return 1
 
     if not args.slack_webhook_url:
@@ -193,12 +204,13 @@ def main() -> int:
         return 1
 
     jeez = get_json_of_pipelinerun(args.pipelinerun)
-    namespace = jeez['metadata']['namespace']
+    namespace = jeez["metadata"]["namespace"]
     if args.log_url == "" and args.openshift.lower() == "true":
         # TODO: Add tekton dashboard if we can find this automatically
         try:
-            args.log_url = get_openshift_console_url(namespace) + \
-                args.pipelinerun + "/logs"
+            args.log_url = (
+                get_openshift_console_url(namespace) + args.pipelinerun + "/logs"
+            )
         except subprocess.CalledProcessError:
             args.log_url = ""
     elif args.openshift.lower() == "true" and args.log_url != "":
@@ -222,13 +234,14 @@ def main() -> int:
     if not failures and args.report_success.lower() != "true":
         return 0
 
-    ret = send_slack_message(args.slack_webhook_url, slack_subject, slack_text,
-                             slack_image)
+    ret = send_slack_message(
+        args.slack_webhook_url, slack_subject, slack_text, slack_image
+    )
     if ret:
         print(ret)
 
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
